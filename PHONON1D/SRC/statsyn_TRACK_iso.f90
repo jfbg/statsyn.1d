892c5ff319349a09b68855ebe49bf3d93e6e766e
jguertin <jguertin@fd5bf823-7f4c-4a25-bc5e-1c6d123694fa>
2012-05-28 18:30:31 +0000

    Made all vars global + routine backup

--- PHONON1D/SRC/statsyn_TRACK_iso.f90
+++ PHONON1D/SRC/statsyn_TRACK_iso.f90
@@ -18,100 +18,108 @@ PROGRAM statsyn_TRACK_iso
 ! Add some line of characters here to test svn
 
       IMPLICIT NONE
-      INTEGER, PARAMETER :: nlay0=1000, nt0=144000, nx0=91
-      REAL          z(nlay0),vf(nlay0,2),rh(nlay0)
-      REAL          z_s(nlay0),r_s(nlay0),vs(nlay0,2)
-      REAL          t,x,xo,a,w(nt0)
-      CHARACTER*100 IFile,ofile,ofile2
-      REAL          dx1,dt1
-      INTEGER       irtr1
-      INTEGER     :: iz,iz1,itt
-			REAL  			:: maxcount
-      INTEGER     :: IT,JT,I,J,ic,jj,k,kk,ll,mm
-      REAL          p,ang1
-      DOUBLE PRECISION wf(nx0,nt0,3)        !STACKED DATA
-      REAL          Q(nlay0)              !QUALITY FACTOR 
-      REAL          dtstr1                !ATTENUATION PER LAYER
-      REAL          mt(nt0)               !SOURCE-TIME FUNCTION 
-      COMPLEX       ms(nt0),ss(nx0,nt0)   !SOURCE & STACKED SPECTRA
-			REAL          nn(nx0,nt0)
-      REAL          pi,P0
-		  INTEGER       n180,idelt1,idelt2
-		  REAL       :: angst                 !! Starting angle for trace
-		  INTEGER		 :: iztrack,ixtrack
-      
-      ! SOURCE
-      REAL          mts(101,4,nt0)        !ATTENUATED SOURCE
-      REAL          b(nt0), e(nt0)        !HILBERT TRANSFORM & ENVELOPE
-      REAL          mtsc(nt0),datt,dtst1  !SCRATCH SPACE
-      INTEGER       ims
-			
-      ! SCATTERING
-			REAL          ds_scat               !Distance between scatterers
-			REAL					dz										!Distance between actual depth and base of layer
-			REAL					ds_SL									!Distance between phonon and next velocity layer
-			REAL          dh										!Vertical Distance between phonon and next vel layer.
-			
-			! ENERGY TRACKING
-			CHARACTER*100 :: tfile
-			DOUBLE PRECISION,ALLOCATABLE,DIMENSION(:,:,:) :: trackcount !Phonon Tracking array
-			REAL			::	attn, minattn
-			INTEGER			:: nttrack		!track time points
-			INTEGER		::	nttrack_dt						!time interval for saving phonon position
-			REAL			::	normfactor						!Normalization factor for cell size
 
-      
-      INTEGER       ncaust,icaust         !NUMBER OF CAUSTICS IN A RAY TRACE
-      INTEGER       ud
-      
-      REAL          d2r,re,rm,deg2km
-      INTEGER       EorM                  !1=EARTH, 2=MOON
-      
-      REAL          frac
-      REAL          erad
-      
-      REAL          arp,ars,atp,ats,ar,at !P- & S-WAVE REFL & TRANS COEFS
-      REAL          rt_sum,rt_min,rt_max  !MAX & MIN REFL PROBABILITIES
-      
-      INTEGER       ip,ip0                !1=P, 2=SH, 3=SV
-      REAL          x_sign
-      
-      REAL          scat_depth,scat_prob
-      REAL          scat_thet,scat_phi
-      REAL          az
-      REAL          dp
-      REAL        :: d
-      REAL        :: delta
-      REAL        :: dxi
-      REAL        :: h     !! Layer thickness
-      INTEGER     :: idum
-      INTEGER     :: imth  !! Interpolation method (1 or 2)
-      INTEGER     :: iwave !! (P(2) or S(2))
-      INTEGER     :: ix,nx ,ixtemp,ixdeg   !! Index & number of distances
-      INTEGER     :: nfil  !! Number of filter values for hilbert transform
-      INTEGER     :: ntr   !! Number of traces
-      INTEGER     :: nts,nts1   !! Number of time series points for source
-      INTEGER     :: nitr  !! Number of ith trace (last)
-      INTEGER     :: nt    !! Number of time in output file
-      INTEGER     :: nlay  !! Number of layers in model
-      REAL        :: r0,r1    !! random number 0-1
-      REAL        :: pow2,pow1 !! Normalization factor for hilber transform
-      REAL        :: s,s1,s2     !! Attenuation & bounds on attenuation for distance
-      REAL        :: scr1,scr2,scr3,scr4 !! Flat earth approximation variables
-      REAL        :: t0,t1,t2,dti,t_last  !! Time variables (bounds & interval)
-      REAL        :: ubot, utop !! Bottom & Top slowness
-      REAL        :: x1, x2     !! Distance bounds
-      
-      REAL           c_mult(3)
-      CHARACTER*3    cmp(3)
-      REAL           p1,p2(2)              !Ray parameters
-      REAL           qdep
-      
-      INTEGER        status                !I/O ERROR (0=no READ error)
-      INTEGER        n_iter_last,it_last,ix_last
-      INTEGER     :: nseed
-      INTEGER     :: seed
-      INTEGER (kind=8)     :: nclock,nclock1
+			MODULE pho_vars			! Make variables global
+				save
+							
+				INTEGER, PARAMETER :: nlay0=1000, nt0=144000, nx0=91
+				REAL          z(nlay0),vf(nlay0,2),rh(nlay0)
+				REAL          z_s(nlay0),r_s(nlay0),vs(nlay0,2)
+				REAL          t,x,xo,a,w(nt0)
+				CHARACTER*100 IFile,ofile,ofile2
+				REAL          dx1,dt1
+				INTEGER       irtr1
+				INTEGER     :: iz,iz1,itt
+				REAL  			:: maxcount
+				INTEGER     :: IT,JT,I,J,ic,jj,k,kk,ll,mm
+				REAL          p,ang1
+				DOUBLE PRECISION wf(nx0,nt0,3)        !STACKED DATA
+				REAL          Q(nlay0)              !QUALITY FACTOR 
+				REAL          dtstr1                !ATTENUATION PER LAYER
+				REAL          mt(nt0)               !SOURCE-TIME FUNCTION 
+				COMPLEX       ms(nt0),ss(nx0,nt0)   !SOURCE & STACKED SPECTRA
+				REAL          nn(nx0,nt0)
+				REAL          pi,P0
+				INTEGER       n180,idelt1,idelt2
+				REAL       :: angst                 !! Starting angle for trace
+				INTEGER		 :: iztrack,ixtrack
+				
+				! SOURCE
+				REAL          mts(101,4,nt0)        !ATTENUATED SOURCE
+				REAL          b(nt0), e(nt0)        !HILBERT TRANSFORM & ENVELOPE
+				REAL          mtsc(nt0),datt,dtst1  !SCRATCH SPACE
+				INTEGER       ims
+				
+				! SCATTERING
+				REAL          ds_scat               !Distance between scatterers
+				REAL					dz										!Distance between actual depth and base of layer
+				REAL					ds_SL									!Distance between phonon and next velocity layer
+				REAL          dh										!Vertical Distance between phonon and next vel layer.
+				REAL					iz2										!Temp var to find location of phonon
+				
+				! ENERGY TRACKING
+				CHARACTER*100 :: tfile
+				DOUBLE PRECISION,ALLOCATABLE,DIMENSION(:,:,:) :: trackcount !Phonon Tracking array
+				REAL			::	attn, minattn
+				INTEGER			:: nttrack		!track time points
+				INTEGER		::	nttrack_dt						!time interval for saving phonon position
+				REAL			::	normfactor						!Normalization factor for cell size
+	
+				
+				INTEGER       ncaust,icaust         !NUMBER OF CAUSTICS IN A RAY TRACE
+				INTEGER       ud
+				
+				REAL          d2r,re,rm,deg2km
+				INTEGER       EorM                  !1=EARTH, 2=MOON
+				
+				REAL          frac
+				REAL          erad
+				
+				REAL          arp,ars,atp,ats,ar,at !P- & S-WAVE REFL & TRANS COEFS
+				REAL          rt_sum,rt_min,rt_max  !MAX & MIN REFL PROBABILITIES
+				
+				INTEGER       ip,ip0                !1=P, 2=SH, 3=SV
+				REAL          x_sign
+				
+				REAL          scat_depth,scat_prob
+				REAL          scat_thet,scat_phi
+				REAL          az
+				REAL          dp
+				REAL        :: d
+				REAL        :: delta
+				REAL        :: dxi
+				REAL        :: h     !! Layer thickness
+				INTEGER     :: idum
+				INTEGER     :: imth  !! Interpolation method (1 or 2)
+				INTEGER     :: iwave !! (P(2) or S(2))
+				INTEGER     :: ix,nx ,ixtemp,ixdeg   !! Index & number of distances
+				INTEGER     :: nfil  !! Number of filter values for hilbert transform
+				INTEGER     :: ntr   !! Number of traces
+				INTEGER     :: nts,nts1   !! Number of time series points for source
+				INTEGER     :: nitr  !! Number of ith trace (last)
+				INTEGER     :: nt    !! Number of time in output file
+				INTEGER     :: nlay  !! Number of layers in model
+				REAL        :: r0,r1    !! random number 0-1
+				REAL        :: pow2,pow1 !! Normalization factor for hilber transform
+				REAL        :: s,s1,s2     !! Attenuation & bounds on attenuation for distance
+				REAL        :: scr1,scr2,scr3,scr4 !! Flat earth approximation variables
+				REAL        :: t0,t1,t2,dti,t_last  !! Time variables (bounds & interval)
+				REAL        :: ubot, utop !! Bottom & Top slowness
+				REAL        :: x1, x2     !! Distance bounds
+				
+				REAL           c_mult(3)
+				CHARACTER*3    cmp(3)
+				REAL           p1,p2(2)              !Ray parameters
+				REAL           qdep
+				
+				INTEGER        status                !I/O ERROR (0=no READ error)
+				INTEGER        n_iter_last,it_last,ix_last
+				INTEGER     :: nseed
+				INTEGER     :: seed
+				INTEGER (kind=8)     :: nclock,nclock1
+			END MODULE pho_vars
+			
+			USE
 !			^^^^^ DECLARATIONS ^^^^^
 
 
@@ -546,10 +554,10 @@ PROGRAM statsyn_TRACK_iso
 							
 
 							
-                ! ============ >>
-                ! SCATTERING LAYER
-                NITR = NITR + 1
-                r0 = rand()           !RANDOM NUMBER FROM 0 TO 1
+				! ============ >>
+				! SCATTERING LAYER
+				NITR = NITR + 1
+				r0 = rand()           !RANDOM NUMBER FROM 0 TO 1
        
 			 
                 IF (z_s(iz) <= scat_depth) THEN
@@ -633,14 +641,21 @@ PROGRAM statsyn_TRACK_iso
 
 					
 					! Calculate new ds_SL based on new ud (direction)
+						iz2 = 1
+						DO WHILE (z_act > z_s(iz2+1))               !FIND WHICH LAYER PHONON IS ON
+						 iz2 = iz2 +1															 ! FIRST LAYER IS ASSUMED TO BE AT 0km.
+						END DO
+						iz = iz2
 						IF (ud == 1) dh = abs(z_act - z_s(iz-1)) ! Distance to vel layer above
-						IF (ud == -1) dh = abs(z_act - z_s(iz+1))  ! Distance to vel layer below
+						IF (ud == -1) dh = abs(z_act - z_s(iz))  ! Distance to vel layer below
 						CALL LAYERTRACE(p,dh,utop,ubot,imth,dx1,dt1,irtr1)
 						ds_SL = ((z_s(iz)-z_s(iz-1))**2+dx1**2)**0.5
 					
 				END DO 
 				
-				!Leaves while loop when ds_SL < distance to next vel layer                
+				!Leaves while loop when ds_SL < distance to next vel layer
+				!Need to travel to next vel layer
+				!Already called LAYERTRACE to check ds_SL, can use this to make phonon travels.		                
   ! ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ !
 	!!!!! BEING EDITED ==================================================
                 					END IF !SCATTERING LAYER IF
