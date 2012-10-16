6d48d8d52298834ba19e12a02bd25680e1d48ba8
jguertin <jguertin@fd5bf823-7f4c-4a25-bc5e-1c6d123694fa>
2012-10-16 16:15:32 +0000

    D4

--- PHONON1D/SRC/statsyn_globalscat.f90
+++ PHONON1D/SRC/statsyn_globalscat.f90
@@ -503,7 +503,7 @@ PROGRAM STATSYN_GLOBALSCAT
 			 z_act = z(iz+izfac)    !Depth of phonon before ray tracing  FLAT
 
 			 !DEBUG
-       WRITE(78,*) I,NITR,z_act,x,t,az,p,ip,ds_scat,ds_SL,iz,ud
+!       WRITE(78,*) I,NITR,z_act,x,t,az,p,ip,ds_scat,ds_SL,iz,ud
       
 			
 				! ============ >>
@@ -607,7 +607,7 @@ PROGRAM STATSYN_GLOBALSCAT
 
 
 										 !DEBUG
-										 WRITE(78,*) I,NITR,z_act,x,t,az,p,ip,ds_scat,ds_SL,iz,ud
+!										 WRITE(78,*) I,NITR,z_act,x,t,az,p,ip,ds_scat,ds_SL,iz,ud
 																						
 			
 										END DO
@@ -799,26 +799,26 @@ PROGRAM STATSYN_GLOBALSCAT
       wf(1,1,2) = 1.
       wf(1,1,3) = 1.
       
-!      DO ic = 1, 3
-!      ofile2 = trim(ofile)//'.'//cmp(ic)
-!
-!      OPEN(22,FILE=trim(ofile2),STATUS='UNKNOWN')    !OPEN OUTPUT FILE
-!       
-!       WRITE(22,*) nt,nx
-!       WRITE(22,FMT=888) 999.99,(x1+dxi*float(J-1),J=1,nx)
-!      
-!				DO I = 1, nt
-!					DO J = 1, nx
-!						IF (abs(wf(J,I,ic)) > 999.9999) wf(J,I,ic) = 999.9999*wf(J,I,ic)/abs(wf(J,I,ic))
-!					END DO
-!					WRITE(22,FMT=888) t1+float(I-1)*dti,(wf(J,I,ic)*0.1,J=1,nx)
-!				END DO
-!
-!      
-!				CLOSE(22)
-!				
-!				
-!      END DO
+      DO ic = 1, 3
+      ofile2 = trim(ofile)//'.'//cmp(ic)
+
+      OPEN(22,FILE=trim(ofile2),STATUS='UNKNOWN')    !OPEN OUTPUT FILE
+       
+       WRITE(22,*) nt,nx
+       WRITE(22,FMT=888) 999.99,(x1+dxi*float(J-1),J=1,nx)
+      
+				DO I = 1, nt
+					DO J = 1, nx
+						IF (abs(wf(J,I,ic)) > 999.9999) wf(J,I,ic) = 999.9999*wf(J,I,ic)/abs(wf(J,I,ic))
+					END DO
+					WRITE(22,FMT=888) t1+float(I-1)*dti,(wf(J,I,ic)*0.1,J=1,nx)
+				END DO
+
+      
+				CLOSE(22)
+				
+				
+      END DO
       WRITE(6,*) 'Synthetic outputs done'
       
       !Debug
