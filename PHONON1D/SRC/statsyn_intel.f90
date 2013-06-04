e8efa23889b5d7ffa5e0d020cde0618a6db885ae
jguertin <jguertin@fd5bf823-7f4c-4a25-bc5e-1c6d123694fa>
2013-06-04 03:30:14 +0000

    etime to cpu_time in intel version

--- PHONON1D/SRC/statsyn_intel.f90
+++ PHONON1D/SRC/statsyn_intel.f90
@@ -1371,6 +1371,7 @@ SUBROUTINE SURFACE_PSV_BEN
 ! Calculate P-SV reflection coefficients at free surface based on BEN-MENAHEM (p.480)
 
       USE pho_vars
+      USE IFPORT
       IMPLICIT NONE
       
       COMPLEX(8)    velP,velS,angP,angS,D1
@@ -1524,6 +1525,9 @@ END SUBROUTINE RTCOEF_SH
 !
 SUBROUTINE RTCOEF_PSV(pin,vp1,vs1,den1,vp2,vs2,den2, &
                          rrp,rrs,rtp,rts,ip,ud,amp,cons_EorA)
+                         
+      USE IFPORT
+                         
       IMPLICIT     NONE
       REAL(8)      vp1,vs1,den1,vp2,vs2,den2     !VELOCITY & DENSITY
       INTEGER      ip                            !P (1) OR S (2)                          
@@ -1654,6 +1658,7 @@ END SUBROUTINE RTCOEF_PSV
 SUBROUTINE RTFLUID_BEN_S2L(realp,ip,ra,rb,rc,rrhos,rrhof,amp,ud,cons_EorA,I)
 
 ! Going from Mantle to Core, solid to liquid
+      USE IFPORT
 
       IMPLICIT NONE
 
@@ -1775,6 +1780,8 @@ END SUBROUTINE RTFLUID_BEN_S2L
 SUBROUTINE RTFLUID_BEN_L2S(realp,ip,ra,rb,rc,rrhos,rrhof,amp,ud,cons_EorA)
 
 ! Going from Core to Mantle, liquid to solid
+
+      USE IFPORT
 
       IMPLICIT NONE
 
@@ -2599,6 +2606,8 @@ SUBROUTINE GET_DS_SCAT
       !The output dscat has been
       
       USE pho_vars      
+      USE IFPORT
+      
       IMPLICIT NONE
       REAL(8)     rt,z_ft,fac,bg_sl,z_nf
       
@@ -2674,6 +2683,7 @@ END SUBROUTINE GET_DS_SL
 
 SUBROUTINE REF_TRAN_PROB(p,az,iz_scat,x_sign,ud,iwave,ip,vel_perturb,vf,conv_count,rh,cons_EorA)
 !      USE pho_vars
+      USE IFPORT
       IMPLICIT NONE
       
       INTEGER, PARAMETER :: nlay0=1000
