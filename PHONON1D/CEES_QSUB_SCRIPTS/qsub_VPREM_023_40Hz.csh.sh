faeda60d460b9a9c559642c23ae77ae4a5212f43
jguertin <jguertin@fd5bf823-7f4c-4a25-bc5e-1c6d123694fa>
2013-06-21 20:04:25 +0000

    CU

--- PHONON1D/CEES_QSUB_SCRIPTS/qsub_VPREM_023_40Hz.csh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/tcsh
-#PBS -N VPREM_023_40Hz.csh
-#PBS -l nodes=1:ppn=16
-#PBS -q jfl
-#PBS -V
-cd $PBS_O_WORKDIR
-
-csh SCRIPTS_CEES/VPREM_023_40Hz.csh.csh
--- PHONON1D/CEES_QSUB_SCRIPTS/qsub_VPREM_nVL_001B_07Hz.csh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/tcsh
-#PBS -N VPREM_nVL_001B_07Hz.csh
-#PBS -l nodes=1:ppn=16
-#PBS -q jfl
-#PBS -V
-cd $PBS_O_WORKDIR
-
-csh SCRIPTS_CEES/VPREM_nVL_001B_07Hz.csh.csh
