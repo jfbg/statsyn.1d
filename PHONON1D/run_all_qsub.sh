d690fed47f6cb2aa5683a3a6c9cdab6b2002e522
jguertin <jguertin@fd5bf823-7f4c-4a25-bc5e-1c6d123694fa>
2013-10-31 02:21:44 +0000

    CSMOON-noscat

--- PHONON1D/run_all_qsub.sh
+++ PHONON1D/run_all_qsub.sh
@@ -1,11 +1,21 @@
 #!/bin/tcsh
 
 
-qsub CEES_QSUB_SCRIPTS/qsub_CVPREM_003_07Hz.sh
+qsub CEES_QSUB_SCRIPTS/qsub_CSMOON_001.sh
 sleep 4
-qsub CEES_QSUB_SCRIPTS/qsub_CVPREM_002_07Hz.sh
+qsub CEES_QSUB_SCRIPTS/qsub_CSMOON_005a.sh
 sleep 4
-qsub CEES_QSUB_SCRIPTS/qsub_CVPREM_001_07Hz.sh
+qsub CEES_QSUB_SCRIPTS/qsub_CSMOON_006a.sh
 sleep 4
-qsub CEES_QSUB_SCRIPTS/qsub_CVPREM_001b_07Hz.sh
+qsub CEES_QSUB_SCRIPTS/qsub_CSMOON_007a.sh
+sleep 4
+qsub CEES_QSUB_SCRIPTS/qsub_CSMOON_008a.sh
+sleep 4
+qsub CEES_QSUB_SCRIPTS/qsub_CSMOON_010a.sh
+sleep 4
+qsub CEES_QSUB_SCRIPTS/qsub_CSMOON_010b.sh
+sleep 4
+qsub CEES_QSUB_SCRIPTS/qsub_CSMOON_011a.sh
+sleep 4
+qsub CEES_QSUB_SCRIPTS/qsub_CSMOON_012a.sh
 sleep 4
